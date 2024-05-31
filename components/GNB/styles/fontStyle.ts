5aa0df68aa5fb461920f216c2751715a3ba47a01
LeeSoonGu <rntnsl9280@naver.com>
2024-05-31 17:06:29 +0900

    design: header 공통 폰트 스타일 추가

---
+++ components/GNB/styles/fontStyle.ts
@@ -0,0 +1,33 @@
+import { css } from "@emotion/react";
+
+export const CaptionStyle = css`
+  font-family: "Spoqa Han Sans Neo";
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 16px;
+`;
+
+export const BodyRegular = css`
+  font-family: "Spoqa Han Sans Neo";
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 22px;
+`;
+export const BodyBold = css`
+  font-family: "Spoqa Han Sans Neo";
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+export const H2 = css`
+  font-family: "Spoqa Han Sans Neo";
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: 0.48px;
+`;

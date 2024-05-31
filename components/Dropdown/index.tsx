d8d9a62b8054fecf4f137ac08c6039a90e1986d6
우지석 <jisurk@gmail.com>
2024-05-31 17:13:57 +0900

    feat: 외부 클릭시 열려있는 options창이 닫히는 기능 추가

--- components/Dropdown/index.tsx
+++ components/Dropdown/index.tsx
@@ -1,47 +1,91 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import Image from "next/image";
 import styles from "./Dropdown.module.scss";
+import ArrowUpIcon from "../../public/image/icon/ArrowUp.svg";
+import ArrowDownIcon from "../../public/image/icon/ArrowDown.svg";
 
 interface DropdownProps {
   options: string[];
 }
 
 function Dropdown({ options }: DropdownProps) {
-  const [selectedOptions, setSelectedOptions] = useState("선택");
+  const [selectedOption, setSelectedOption] = useState("선택");
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
+
   const handleOptionClick = (option: string) => {
-    setSelectedOptions(option);
+    setSelectedOption(option);
     setIsOpen(false);
   };
 
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
   return (
-    <div>
-      <div className={styles["dropdown"]}>
+    <div
+      className={styles["dropdown"]}
+      ref={dropdownRef as React.RefObject<HTMLDivElement>}
+    >
+      <div
+        className={styles["dropdown__inputWrapper"]}
+        onClick={toggleDropdown}
+      >
         <input
           type="text"
           className={styles["dropdown__input"]}
           placeholder="선택"
-          value={selectedOptions}
-          onClick={toggleDropdown}
+          value={selectedOption}
           readOnly
         />
-        {isOpen && (
-          <ul className={styles["dropdown__optionBox"]}>
-            {options.map((option) => (
-              <li
-                key={option}
-                className={styles["dropdown__option"]}
-                onClick={() => handleOptionClick(option)}
-              >
-                {option}
-              </li>
-            ))}
-          </ul>
+        {isOpen ? (
+          <Image
+            src={ArrowDownIcon}
+            alt="Arrow Down"
+            width={16}
+            height={16}
+            className={styles["dropdown__icon"]}
+          />
+        ) : (
+          <Image
+            src={ArrowUpIcon}
+            alt="Arrow Up"
+            width={16}
+            height={16}
+            className={styles["dropdown__icon"]}
+          />
         )}
       </div>
+      {isOpen && (
+        <ul className={styles["dropdown__optionBox"]}>
+          {options.map((option) => (
+            <li
+              key={option}
+              className={styles["dropdown__option"]}
+              onClick={() => handleOptionClick(option)}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }

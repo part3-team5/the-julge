5602a038f4b4aebce6c5e591804900ff21bc1f73
MEGUMMY1 <cys10902@nate.com>
2024-05-31 22:54:09 +0900

    feat: CustomNoticeList Components(맞춤 공고)

---
+++ components/notice/NoticeList/index.tsx
@@ -0,0 +1,11 @@
+// 메인 화면 공고 목록 컴포넌트
+import styles from "./NoticeList.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
+const NoticeList = () => {
+    return <div></div>;
+};
+
+export default NoticeList;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
+import CustomNoticeList from "@/components/notice/CustomNoticeList";
+import NoticeList from "@/components/notice/NoticeList";
 import Head from "next/head";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.scss";
 
 export default function Home() {
     return (
@@ -11,6 +11,8 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <CustomNoticeList />
+            <NoticeList />
         </>
     );
 }

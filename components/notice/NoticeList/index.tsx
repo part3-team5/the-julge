c10ad74b02a9ad9a2344beb7eccd83d224dec344
MEGUMMY1 <cys10902@nate.com>
2024-06-01 23:34:13 +0900

    design: post 카드 컴포넌트 grid로 변경

--- components/notice/NoticeList/index.tsx
+++ components/notice/NoticeList/index.tsx
@@ -3,19 +3,11 @@ import Post from "@/components/Post";
 import styles from "./NoticeList.module.scss";
 import classNames from "classnames/bind";
 import DropdownSmall from "@/components/DropdownSmall";
+import { posts } from "@/public/postTest";
 
 const cx = classNames.bind(styles);
 
 const NoticeList = () => {
-    const posts = [
-        { startsAt: "2024-06-30T00:00:00Z", workhour: 1 },
-        { startsAt: "2024-05-31T00:00:00Z", workhour: 5 },
-        { startsAt: "2024-07-31T00:00:00Z", workhour: 5 },
-        { startsAt: "2024-07-31T00:00:00Z", workhour: 5 },
-        { startsAt: "2021-07-31T00:00:00Z", workhour: 5 },
-        { startsAt: "2022-07-31T00:00:00Z", workhour: 5 },
-    ];
-
     return (
         <div className={cx("notice__wrapper")}>
             <div className={cx("notice__container")}>
@@ -28,7 +20,12 @@ const NoticeList = () => {
                 </div>
                 <div className={cx("post__grid")}>
                     {posts.map((post, index) => (
-                        <Post key={index} startsAt={post.startsAt} workhour={post.workhour} />
+                        <Post
+                            key={index}
+                            startsAt={post.startsAt}
+                            workhour={post.workhour}
+                            increasePercent={post.increasePercent}
+                        />
                     ))}
                 </div>
             </div>

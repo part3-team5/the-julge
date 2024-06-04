d16eb042c5dfcccef623470df3f13e0f08b2d2b8
우지석 <jisurk@gmail.com>
2024-06-04 15:45:57 +0900

    style: 클래스네이밍 간략하게 수정

--- components/Profile/ProfileEmpty/index.tsx
+++ components/Profile/ProfileEmpty/index.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames/bind";
-import styles from "../profile.module.scss";
+import styles from "../Profile.module.scss";
 import Button from "@/components/Button";
 import { ProfileEmptyProps } from "../profile.types";
 
@@ -8,12 +8,12 @@ const cx = classNames.bind(styles);
 function ProfileEmpty({ onClick }: ProfileEmptyProps) {
   return (
     <div className={cx("profile")}>
-      <h1 className={cx("profile__title")}>내 프로필</h1>
-      <div className={cx("profile__box")}>
-        <p className={cx("profile-description")}>
+      <h1 className={cx("title")}>내 프로필</h1>
+      <div className={cx("box")}>
+        <p className={cx("description")}>
           내 프로필을 등록하고 원하는 가게에 지원해 보세요.
         </p>
-        <div className={cx("profile__button--wrapper")}>
+        <div className={cx("button-wrapper")}>
           <Button onClick={onClick} btnColorType="orange">
             내 프로필 등록하기
           </Button>
--- components/Profile/ProfileForm/index.tsx
+++ components/Profile/ProfileForm/index.tsx
@@ -8,40 +8,36 @@ const cx = classNames.bind(styles);
 
 function ProfileForm() {
   return (
-    <main className={cx(["profile"], ["profile__main"])}>
-      <h1 className={cx("profile__title")}>내 프로필</h1>
-      <form className={cx("profile__form")}>
-        <div className={cx("profile__input-wrapper")}>
-          <section className={cx("profile__input__section")}>
-            <label className={cx("profile__label")} htmlFor="name">
+    <main className={cx(["profile"], ["main"])}>
+      <h1 className={cx("title")}>내 프로필</h1>
+      <form className={cx("form")}>
+        <div className={cx("input-wrapper")}>
+          <section className={cx("input__section")}>
+            <label className={cx("label")} htmlFor="name">
               이름
             </label>
-            <input className={cx("profile__input")} type="text" id="name" />
+            <input className={cx("input")} type="text" id="name" />
           </section>
-          <section className={cx("profile__input__section")}>
-            <label className={cx("profile__label")} htmlFor="phone-number">
+          <section className={cx("input__section")}>
+            <label className={cx("label")} htmlFor="phone-number">
               전화번호
             </label>
-            <input
-              className={cx("profile__input")}
-              type="tel"
-              id="phone-number"
-            />
+            <input className={cx("input")} type="tel" id="phone-number" />
           </section>
-          <section className={cx("profile__input__section")}>
-            <label className={cx("profile__label")} htmlFor="area">
+          <section className={cx("input__section")}>
+            <label className={cx("label")} htmlFor="area">
               선호 지역
             </label>
             <Dropdown options={areaOptions} id="area" />
           </section>
         </div>
-        <section className={cx("profile__textarea__section")}>
-          <label className={cx("profile__label")} htmlFor="introduction">
+        <section className={cx("textarea__section")}>
+          <label className={cx("label")} htmlFor="introduction">
             소개
           </label>
-          <textarea className={cx("profile__textarea")} id="introduction" />
+          <textarea className={cx("textarea")} id="introduction" />
         </section>
-        <div className={cx("profile__button__section")}>
+        <div className={cx("button__section")}>
           <div className={cx("button-wrapper")}>
             <Button btnColorType="orange">프로필 등록</Button>
           </div>

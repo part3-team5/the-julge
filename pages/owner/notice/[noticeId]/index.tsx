c7e733e8b040ac746463c263e8f9cd5accf94cf4
eunjae <rladmswo1715@naver.com>
2024-06-05 14:28:05 +0900

    rename: 공고상페이지 url 변경

--- pages/owner/notice/[noticeId]/index.tsx
+++ pages/owner/notice/[noticeId]/index.tsx
@@ -1,60 +1,23 @@
-import Modal from "@/components/Modal";
 import styles from "./DetailedOwnerNotice.module.scss";
 import classNames from "classnames/bind";
-import { useModal } from "@/hooks/useModal";
-import Button from "@/components/Button";
+import NoticeDetailed from "@/components/notice/NoticeDetailed";
 
 const cx = classNames.bind(styles);
 
 const DetailedOwnerNotice = () => {
-  const { openModal, closeModal } = useModal();
-
-  const handleOpenModal = () => {
-    openModal({
-      modalType: "alert",
-      content: "안녕",
-      btnName: ["확인"],
-    });
-  };
-
-  const handleOpenModal2 = () => {
-    openModal({
-      modalType: "select",
-      content: "선택하세요.",
-      btnName: ["아니오", "선택하기"],
-      callBackFnc: handleModalButtonFnc,
-    });
-  };
-
-  const handleOpenModal3 = () => {
-    openModal({
-      modalType: "warning",
-      content: "경고창입니다",
-      btnName: ["확인"],
-    });
-  };
-
-  const handleModalButtonFnc = () => {
-    alert("선택되었습니다.");
-    closeModal();
-  };
-
   return (
     <>
       <div className={cx("content-wrap")}>
-        <section className={cx("notice")}>
-          <Button btnColorType="orange" onClick={handleOpenModal}>
-            모달창 열기
-          </Button>
-          <Button btnColorType="orange" onClick={handleOpenModal2}>
-            모달창 열기2
-          </Button>
-          <Button btnColorType="orange" onClick={handleOpenModal3}>
-            모달창 열기3
-          </Button>
-        </section>
+        <NoticeDetailed />
 
-        <section className={cx("recentlt-viewed")}></section>
+        <section className={cx("applicant-list")}>
+          <h2 className={cx("notice--head__name")}>신청자 목록</h2>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </section>
       </div>
     </>
   );

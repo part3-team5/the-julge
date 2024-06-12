95cb6ae100477a704ebc63f96d46deced72f8edc
Woojiseok <jisurk@gmail.com>
2024-06-12 22:04:25 +0900

    fea: Application 조건부 렌더링 추가

--- components/Application/index.tsx
+++ components/Application/index.tsx
@@ -1,9 +1,38 @@
-// import ApplicationEmpty from "./Empty";
-import ApplicationView from "./View";
-// import View from "./View";
+import { useEffect, useState } from "react";
+import ApplicationView from "./ApplicationView";
+import { getUserId } from "@/utils/jwt";
+import { instance } from "@/utils/instance";
+import { ApplicationResponse } from "./Application.types";
+import ApplicationEmpty from "./ApplicationEmpty";
 
 function Application() {
-  return <ApplicationView />;
+  const [hasApplications, setHasApplications] = useState(false);
+
+  useEffect(() => {
+    const getApplications = async () => {
+      const userId = getUserId();
+
+      try {
+        const response = await instance.get<ApplicationResponse>(
+          `/users/${userId}/applications`,
+          {
+            params: {
+              offset: 0,
+              limit: 1,
+            },
+          }
+        );
+
+        setHasApplications(response.data.items.length > 0);
+      } catch (error) {
+        console.log("GET ApplicationList Error:", error);
+      }
+    };
+
+    getApplications();
+  }, []);
+
+  return hasApplications ? <ApplicationView /> : <ApplicationEmpty />;
 }
 
 export default Application;

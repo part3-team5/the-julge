940aec566d9e45d84903277f3f7868349db35756
MEGUMMY1 <cys10902@nate.com>
2024-06-13 17:31:26 +0900

    design: Footer Layout 변경

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -2,16 +2,19 @@ import { ReactNode } from "react";
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import style from "./Layout.module.scss";
 
 type Children = {
-  children: ReactNode;
+    children: ReactNode;
 };
 export default function Layout({ children }: Children) {
-  return (
-    <>
-      <Header />
-      <div>{children}</div>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <div className={style.wrap}>
+                <Header />
+                <div className={style.body}>{children}</div>
+                <Footer />
+            </div>
+        </>
+    );
 }

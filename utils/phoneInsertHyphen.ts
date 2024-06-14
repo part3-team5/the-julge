170811581c42e127f3e4c3242be87092cd959471
우지석 <jisurk@gmail.com>
2024-06-14 12:54:07 +0900

    feat: Input에 전화번호적을때 - 자동생성 기능 추가

--- utils/phoneInsertHyphen.ts
+++ utils/phoneInsertHyphen.ts
@@ -2,7 +2,10 @@ const phoneInsertHyphen = (phoneNumber: string | undefined) => {
   const returnPhoneNumber = phoneNumber
     ? phoneNumber
         .replace(/[^0-9]/g, "")
-        .replace(/^(\d{3})(\d{4})(\d{4})$/, `$1-$2-$3`)
+        .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, (_, g1, g2, g3) => {
+          const groups = [g1, g2, g3].filter((group) => group !== "");
+          return groups.join("-");
+        })
     : "";
 
   return returnPhoneNumber;

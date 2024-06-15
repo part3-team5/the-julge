c7e972711fb69964d272c69f217481a458642994
MEGUMMY1 <cys10902@nate.com>
2024-06-15 22:34:43 +0900

    delete: 미사용 파일 삭제

--- pages/api/auth.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function authHandler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { jwt = "", userType = "", id = "" } = req.cookies;
-  res.status(200).json({ jwt, userType, id });
-}

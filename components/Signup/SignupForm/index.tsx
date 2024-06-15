68daca139c32693c1909771ceadefe051cb2fe2e
LeeSoonGu <rntnsl9280@naver.com>
2024-06-15 16:38:49 +0900

    refactor: 회원가입 모달 적용

--- components/Signup/SignupForm/index.tsx
+++ components/Signup/SignupForm/index.tsx
@@ -16,6 +16,8 @@ import {
 import { useRouter } from "next/router";
 import { emailRegex, passwordRegex } from "@/utils/signupRegex";
 import { BASE_URL } from "@/constants/constants";
+import ConfirmModal from "@/components/Modal/ModalContent/AlertModal";
+import { IModalProps } from "@/components/Modal/Modal.types";
 
 export default function SignupForm() {
   const [type, setType] = useState<UserType>(UserType.PART_TIME);
@@ -29,23 +31,32 @@ export default function SignupForm() {
   } = useForm<SignupFormData>({ mode: "onChange" });
   const router = useRouter();
 
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [modalData, setModalData] = useState<IModalProps>({
+    modalType: "alert",
+    content: "",
+    btnName: ["확인"],
+  });
+
   const onSubmit = async (formData: SignupFormData) => {
     try {
-      // 회원가입 데이터 유효성 검사
       const isValid = validateSignupData(formData);
       if (!isValid) return;
 
-      // 회원가입 요청 보내기
       const { email, password } = formData;
       const request = { email, password, type };
       await axios.post(`${BASE_URL}/users`, request);
 
-      // 회원가입 완료 시 Recoil 상태 업데이트 및 페이지 이동
       setSignupState(formData);
-      alert("가입이 완료되었습니다!");
-      router.push("/signin");
+
+      setModalData({
+        modalType: "alert",
+        content: "가입이 완료되었습니다!",
+        btnName: ["확인"],
+        callBackFnc: () => router.push("/signin"),
+      });
+      setModalOpen(true);
     } catch (error) {
-      // 에러 처리
       if (axios.isAxiosError(error) && error.response) {
         const { message } = error.response.data;
         alert(message);
@@ -57,43 +68,53 @@ export default function SignupForm() {
     }
   };
 
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   return (
-    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-      <Input
-        label="이메일"
-        error={errors?.email ? errors.email : undefined}
-        register={register("email", {
-          required: "이메일을 입력하세요.",
-          pattern: {
-            value: emailRegex,
-            message: INVALID_EMAIL,
-          },
-        })}
-      />
-      <Input
-        label="비밀번호"
-        error={errors?.password ? errors.password : undefined}
-        type="password"
-        register={register("password", {
-          required: "비밀번호를 입력하세요.",
-          pattern: {
-            value: passwordRegex,
-            message: INVALID_PASSWORD,
-          },
-        })}
-      />
-      <Input
-        label="비밀번호 확인"
-        error={errors?.passwordCheck ? errors.passwordCheck : undefined}
-        type="password"
-        register={register("passwordCheck", {
-          required: "비밀번호를 다시 입력하세요.",
-          validate: (value) =>
-            value === getValues("password") || "비밀번호가 일치하지 않습니다.",
-        })}
-      />
-      <UserTypeSelect type={type} setType={setType} />
-      <Button btnColorType="orange">가입하기</Button>
-    </form>
+    <>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          label="이메일"
+          error={errors?.email ? errors.email : undefined}
+          register={register("email", {
+            required: "이메일을 입력하세요.",
+            pattern: {
+              value: emailRegex,
+              message: INVALID_EMAIL,
+            },
+          })}
+        />
+        <Input
+          label="비밀번호"
+          error={errors?.password ? errors.password : undefined}
+          type="password"
+          register={register("password", {
+            required: "비밀번호를 입력하세요.",
+            pattern: {
+              value: passwordRegex,
+              message: INVALID_PASSWORD,
+            },
+          })}
+        />
+        <Input
+          label="비밀번호 확인"
+          error={errors?.passwordCheck ? errors.passwordCheck : undefined}
+          type="password"
+          register={register("passwordCheck", {
+            required: "비밀번호를 다시 입력하세요.",
+            validate: (value) =>
+              value === getValues("password") ||
+              "비밀번호가 일치하지 않습니다.",
+          })}
+        />
+        <UserTypeSelect type={type} setType={setType} />
+        <Button btnColorType="orange">가입하기</Button>
+      </form>
+      {isModalOpen && (
+        <ConfirmModal modalData={modalData} closeFunction={closeModal} />
+      )}
+    </>
   );
 }
